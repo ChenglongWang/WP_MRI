b5618ac54c34373d166b40079e3d04fbf1fcd4d9
ChenglongWang <anjingdelonglong@gmail.com>
2013-08-02 23:12:51 +0800

    1.删除站点域名中/wpc路径.此步骤需要重新配置.将index及.htaccess放到www根目录. 2.将首页从index转为home页面. 3.将twentytwelve作为父主题.

--- wp-content/themes/wpbootstrap/footer.php
+++ wp-content/themes/wpbootstrap/footer.php
@@ -1,13 +1,26 @@
-      <hr>
-
 
       <!-- FOOTER -->
       <footer>
-        <p class="pull-right"><a href="http://www.ecnu.edu.cn" target="_blank">ECNU</a></p>
-        <p>&copy; 2013 Company, Inc. &middot; <a href="#">华东师范大学</a> </p>
+        <div id="footer" class ="container">
+              <div class ="lab-address">
+                  <h5>上海市磁共振重点实验室</h5>
+                  <p>
+                      上海市中山北路3663号华东师范大学 &nbsp  200062
+                      <br/>
+                      电话：021-62233873
+                  </p>
+              </div>
+              <div class ="bottomlinks">
+                  <p>
+                      技术支持：
+                      <br />
+                      华东师范大学老图书馆一楼办公室
+              </div>
+         <div class="pull-right">&copy; 2013 Company, Inc. &middot; <a href="#">华东师范大学</a> </div>
+       </div> 
       </footer>
 
-    </div> <!-- /container -->
+ <!--   </div> <!-- /container -->
 
     <!-- Le javascript
     ================================================== -->
