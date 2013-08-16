1ac673934d008bd003835042c37f5170b3d6f6ff
ChenglongWang <anjingdelonglong@gmail.com>
2013-08-16 14:27:05 +0800

    修改教师介绍页面部分bug.

--- wp-content/themes/wpbootstrap/page-teachers.php
+++ wp-content/themes/wpbootstrap/page-teachers.php
@@ -14,48 +14,64 @@
               if($args[0][0]['position'] == 'MRI') array_push($mris, get_the_ID ());
               if($args[0][0]['position'] == 'external') array_push($externals, get_the_ID ());
               if($args[0][0]['position'] == 'student') array_push($students, get_the_ID ());
+              echo "ID".get_the_ID();
           }endwhile;
     ?>
     <p class='subtitle'>行政</p>
+    <table>
         <tr>
             <?php if(!empty($admins)) { ?>
-                <?php foreach($admins as $admin) ?>
-                    <td width="25%"><a class="data"><?php echo get_the_title($admin)?>
+                <?php foreach($admins as $admin) { ?>
+                    <td width="25%"><a href="<?php echo esc_url(get_post_permalink($mri)) ?>" class="data"><?php echo get_the_title($admin)?>
+                <?php } ?>
             <?php } ?></a>
             </td>
         </tr>
+    </table>
     <p class='subtitle'>核磁共振波谱学</p>
+    <table>
         <tr>
             <?php if(!empty($nmrs)) { ?>
-                <?php foreach($nmrs as $nmr) ?>
-                    <td width="25%"><nobr class="data"><?php echo get_the_title($nmr)?>
-            <?php } ?></nobr>
+                <?php foreach($nmrs as $nmr){ ?>
+                    <td width="25%"><a href="<?php echo esc_url(get_post_permalink($mri)) ?>" class="data"><?php echo get_the_title($nmr)?>
+                 <?php } ?>
+            <?php } ?></a>
             </td>
         </tr>
+    </table>
     <p class='subtitle'>磁共振成像技术研发</p>
+    <table
         <tr>
             <?php if(!empty($mris)) { ?>
-                <?php foreach($mris as $mri) ?>
+                <?php foreach($mris as $mri){ ?>
             <td width="25%"><a href="<?php echo esc_url(get_post_permalink($mri)) ?>" class="data"><?php echo get_the_title($mri)?>
+                <?php } ?>
             <?php } ?></a>
             </td>
         </tr> 
+    </table>
     <p class='subtitle'>外聘</p>
-        <tr>
+       <table>
+           <tr>
             <?php if(!empty($externals)) { ?>
-                <?php foreach($externals as $external) ?>
-                    <td width="25%"><nobr class="data"><?php echo get_the_title($external)?>
-            <?php } ?></nobr>
-            </td>
-        </tr>
-    <p class='subtitle'>学生</p>
-        <tr>
-            <?php if(!empty($students)) { ?>
-                <?php foreach($students as $student) ?>
-                    <td width="25%"><a herf="<?php get_post_permalink($student) ?>" class="data"><?php echo get_the_title($student)?>
+                <?php foreach($externals as $external) {?>
+                    <td width="20%"><a href="<?php echo esc_url(get_post_permalink($mri)) ?>" class="data"><?php echo get_the_title($external)?>
+                <?php } ?>            
             <?php } ?></a>
             </td>
         </tr>
+       </table>
+    <p class='subtitle'>学生</p>
+    <table>    
+        <tr>
+            <?php if(!empty($students)) { ?>
+                <?php foreach($students as $student) { ?>
+                    <td width="20%"><a href="<?php echo get_post_permalink($student) ?>" class="data"><?php echo get_the_title($student)?></a></td>
+                <?php } ?>                
+            <?php } ?>
+
+        </tr>
+    </table>
 </section>
 
 <?php get_footer() ?>
