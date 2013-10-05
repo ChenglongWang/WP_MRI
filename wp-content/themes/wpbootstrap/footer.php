d6b0891841a33b1bb0d21d38f66445421f3c2c48
PheobeZheng <bo_li_xie@126.com>
2013-10-05 14:51:07 +0800

    解决footer的bug。

--- wp-content/themes/wpbootstrap/footer.php
+++ wp-content/themes/wpbootstrap/footer.php
@@ -1,26 +1,39 @@
 
 <!-- FOOTER -->
 </div> <!-- /container -->
-<footer style="position: inherit; bottom: 0px; width: 100%; height: 170px; background-color: rgb(51,51,51)">
-    <font color="grey">
-    <div style=" height: 100%; margin-left: 10%; margin-right: 10%">
-        <div class ="lab-address" style="width: 40%; padding-top: 5px;">
-            <h5>上海市磁共振重点实验室</h5>
-            <p>
+<footer style="color: gray; position: inherit; bottom: 0px; width: 100%; height: 170px; background-color: rgb(55,55,55)">
+<!--    <font color="grey">-->
+    <table border="2" style=" width: 100%; height: 100%; margin-left: 20%; margin-right: 10%">
+        <tr>
+<!--        <div class ="lab-address" style="width: 40%; padding-top: 5px;">-->
+            <td style="padding-top: 20px">    
+                <h5>上海市磁共振重点实验室</h5>
+            </td>
+        </tr>
+        <tr>   
+            <td align="left" width="400px" height="50px">
                 上海市中山北路3663号华东师范大学 &nbsp  200062
-                <br/>
+                    <br/>
                 电话：021-62233873
-            </p>
-        </div>
-        <div class ="bottomlinks" style="  width: 30%; ">
-            <p>
-                技术支持：
-                <br />
-                华东师范大学老图书馆一楼办公室
-        </div>
-        <div  style="float: right; width: 30%; padding-bottom: 5px;">&copy; 2013 ECNU, Inc. &middot; <a href="#">华东师范大学</a> </div>
-    </div> 
-    </font>
+            </td>
+<!--        </div>-->
+      
+      
+<!--        <div class ="bottomlinks" style="  width: 30%; ">-->
+            <td> 
+               技术支持：
+                <br/>
+               华东师范大学老图书馆一楼办公室
+            </td>
+<!--        </div>-->
+        </tr>
+        <tr>
+            <td style="float: left; padding-top: 10px">
+               &copy; 2013 ECNU, Inc. &middot; <a href="#">华东师范大学</a> 
+            </td> 
+        </tr>
+    </table> 
+<!--    </font>-->
 </footer>
 
 
